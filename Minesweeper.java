c958386d61f9b480676ee64e7e68c76a6fa61ef6
lleeooaa <1155158173@link.cuhk.edu.hk>
2024-01-31 01:05:15 +0800

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class Minesweeper extends JFrame {
     private JButton[][] boardButtons;
+    private Cell[][] gameboard;
 
     public Minesweeper() {
         setTitle("Minesweeper");
@@ -15,6 +16,7 @@ public class Minesweeper extends JFrame {
         // Create the game board with buttons
         int rows = 24;
         int cols = 24;
+        gameboard = new Cell[24][24];
         boardButtons = new JButton[rows][cols];
         JPanel boardPanel = new JPanel(new GridLayout(rows+1, cols));
 
@@ -27,57 +29,43 @@ public class Minesweeper extends JFrame {
         }
 
         add(boardPanel, BorderLayout.CENTER);
-        setSize(600, 600);
+        setSize(600, 625);
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
     private class GameBoard {
-        private int row;
-        private int col;
-        private Cell[][] gameboard;
-        private int mine_num;
 
-        public GameBoard(int row, int col, int mine_num) {
-            this.row=row;
-            this.col=col;
-            this.mine_num=mine_num;
+        public GameBoard(int row, int col, int mine_num, int start_x , int start_y) {
+            Random rand = new Random();
+            List<Integer> choose = new ArrayList<Integer>();
             for(int i=0;i<row;i++){
                 for(int j=0;j<col;j++){
                     gameboard[i][j]=new Cell(i,j);
-                }
-            }
-            
-        }
-
-        private void init(int start_x, int start_y) {
-            Random rand = new Random();
-            List<Integer> choose = new ArrayList<Integer>();
-            for(int i=0;i<this.row;i++){
-                for(int j=0;j<this.col;j++){
                     choose.add(i*24+j);
                 }
             }
             choose.removeAll(get_neighbour(start_x, start_y));
-            for (int i = 0; i < this.mine_num ; i++) {
+            for (int i = 0; i < mine_num ; i++) {
                 int randomIndex = rand.nextInt(choose.size());
                 int randomElement = choose.get(randomIndex);
-                this.gameboard[Math.floorDiv(randomElement, 24)][randomElement%24].type="mine";
+                gameboard[Math.floorDiv(randomElement, 24)][randomElement%24].type="mine";
                 choose.remove(randomIndex);
             }
-            for(int i=0;i<this.row;i++){
-                for(int j=0;j<this.col;j++){
+            for(int i=0;i<row;i++){
+                for(int j=0;j<col;j++){
                     List<Integer> neighbour=get_neighbour(i, j);
                     int mine=0;
                     for (int k = 0; k < neighbour.size() ; k++) {
-                        if (this.gameboard[Math.floorDiv(neighbour.get(k), 24)][neighbour.get(k)%24].type=="mine")
+                        if (gameboard[Math.floorDiv(neighbour.get(k), 24)][neighbour.get(k)%24].type=="mine")
                             mine++;
                     }
                     if (mine >0)
-                        this.gameboard[i][j].type=Integer.toString(mine);
+                        gameboard[i][j].type=Integer.toString(mine);
                 }
             }
         }
+
 
         private List<Integer> get_neighbour(int x, int y) {
             List<Integer> output = new ArrayList<Integer>();
@@ -119,6 +107,8 @@ public class Minesweeper extends JFrame {
     private class ButtonClickListener implements ActionListener {
         private int row;
         private int col;
+        private boolean started = false;
+      
 
         public ButtonClickListener(int row, int col) {
             this.row = row;
@@ -130,7 +120,14 @@ public class Minesweeper extends JFrame {
         public void actionPerformed(ActionEvent e) {
             // Handle button click event
             JButton clickedButton = (JButton) e.getSource();
-            // TODO: Add your logic for handling the button click and revealing the cell
+            if (clickedButton == boardButtons[row][col] && !gameboard[row][col].revealed) {
+                if (!started) {
+                    new GameBoard(24,24,99,row,col);
+                    started=true;
+                }
+                gameboard[row][col].revealed=true;
+                clickedButton.setEnabled(false);
+            }
         }
     }
 
